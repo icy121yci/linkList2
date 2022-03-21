492afc21e9d5e4922249c9704f22a1fade53b886
icy121yci <101975368+icy121yci@users.noreply.github.com>
2022-03-22 00:20:24 +0800

    dulinkedlist.h
    dulinkedlist.h

---
+++ dulinkedlist.h
@@ -0,0 +1,157 @@
+/***************************************************************************************
+ *	File Name				:	duLinkedList.h
+ *	CopyRight				:	2020 QG Studio
+ *	SYSTEM					:   win10
+ *	Create Data				:	2020.3.28
+ *
+ *
+ *--------------------------------Revision History--------------------------------------
+ *	No	version		Data			Revised By			Item			Description
+ *
+ *
+ ***************************************************************************************/
+
+ /**************************************************************
+*	Multi-Include-Prevent Section
+**************************************************************/
+
+#ifndef DULINKEDLIST_H_INCLUDED
+#define DULINKEDLIST_H_INCLUDED
+
+/**************************************************************
+*	Macro Define Section
+**************************************************************/
+
+#define OVERFLOW -1
+
+/**************************************************************
+*	Struct Define Section
+**************************************************************/
+
+// define element type
+typedef int ElemType;
+
+// define struct of linked list
+typedef struct DuLNode {
+	ElemType data;
+  	struct DuLNode *prior,  *next;
+} DuLNode, *DuLinkedList;
+
+// define status
+typedef enum Status {
+	ERROR,
+	SUCCESS,
+} Status;
+
+
+/**************************************************************
+*	Prototype Declare Section
+**************************************************************/
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status ReverseEvList_DuL(DuLinkedList L);
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status JudCirList_DuL(DuLinkedList L);
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status CirList_DuL(DuLinkedList L);
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status MidList_DuL(DuLinkedList L);
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status ReverseList_DuL(DuLinkedList L);//
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status BuildList_DuL(DuLinkedList *L,int num);//
+
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status InitList_DuL(DuLinkedList *L);//
+
+/**
+ *  @name        : void DestroyList_DuL(DuLinkedList *L)
+ *	@description : destroy a linked list
+ *	@param		 : L(the head node)
+ *	@return		 : status
+ *  @notice      : None
+ */
+void DestroyList_DuL(DuLinkedList *L);//
+
+/**
+ *  @name        : Status InsertBeforeList_DuL(DuLNode *p, LNode *q)
+ *	@description : insert node q before node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertBeforeList_DuL(DuLinkedList L, int num);//
+
+/**
+ *  @name        : Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+ *	@description : insert node q after node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status SearchList_DuL(DuLinkedList L,int num);
+
+/**
+ *  @name        : Status DeleteList_DuL(DuLNode *p, ElemType *e)
+ *	@description : delete the first node after the node p and assign its value to e
+ *	@param		 : p, e
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status DeleteList_DuL(DuLinkedList L, int num);//
+
+/**
+ *  @name        : void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+ *	@description : traverse the linked list and call the funtion visit
+ *	@param		 : L(the head node), visit
+ *	@return		 : Status
+ *  @notice      : None
+ */
+void TraverseList_DuL(DuLinkedList L);//
+
+
+ /**************************************************************
+*	End-Multi-Include-Prevent Section
+**************************************************************/
+#endif
